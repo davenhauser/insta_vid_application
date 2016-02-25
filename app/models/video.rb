ed8664cdc80bc781c46185ecd6ba2927d9e8f817
david niederhauser <niederhauser.dave@gmail.com>
2016-02-24 18:37:07 -0800

    remove 'missing image error' for comments

--- app/models/video.rb
+++ app/models/video.rb
@@ -9,8 +9,6 @@ class Video < ActiveRecord::Base
    validates :video_link, uniqueness: true, presence: true, length: { is: 11 }
    validates :title, presence: true
 
-
-
    def youtube_link
     "https://youtu.be/#{self.video_link}"
   end

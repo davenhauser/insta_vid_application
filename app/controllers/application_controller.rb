72a7079280f295100c4ac0f2e7d67918d00913d8
david niederhauser <niederhauser.dave@gmail.com>
2016-02-25 19:11:02 -0800

    add anchor with + '#comment_message'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
      # Make the current_user method available to views, not just controllers!
     helper_method :current_user
     def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
       def authorize
